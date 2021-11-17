b85989a907c1fbeb34e4d74d45b67fde440d3e9c
LeonardSSH <leonardssh22@gmail.com>
2021-11-18 01:54:54 +0200

    add server & shared types definitions

---
+++ packages/server/enums/colshape.d.ts
@@ -0,0 +1,10 @@
+declare module 'rage-server' {
+	export const enum ColshapeType {
+		CIRCLE = 'circle',
+		CUBOID = 'cuboid',
+		POLYGON = 'polygon',
+		RECTANGLE = 'rectangle',
+		SPHERE = 'sphere',
+		TUBE = 'tube'
+	}
+}
