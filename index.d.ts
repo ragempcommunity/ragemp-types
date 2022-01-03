25457f1d512649cd22b2d6e5fb3258a04c56542d
Ivan Laptop <ivan7k9@gmail.com>
2022-01-03 21:07:10 +0100

    Added [mp.events] interface. Added [mp.events.callProc] function.

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,25 @@
 interface Mp {
+	events: EventMpPool;
+
 	trigger(name: string, ...args: any[]): void;
 	invoke(name: string, ...args: any[]): void;
+}
+
+declare interface EventMpPool {
+	/**
+	 * This function calls registered Remote Prodecure Call (RPC) from browser to client-side and expects a callback.
+	 *
+	 * @param eventName Client name event you want to call.
+	 * @param args Data you want to send to client.
+	 *
+	 * @example
+	 * ```js
+	 * mp.events.callProc('test_proc', 'test');
+	 * ```
+	 *
+	 *
+	 */
+	callProc<PromiseType, ArgsType>(eventName: string, args: ArgsType): Promise<PromiseType>;
 }
 
 interface Window {
