b347e5be967862088c8ff167cd3a5917aecf980d
YouLL <ivan7k9@gmail.com>
2022-01-05 23:54:16 +0100

    Changed [callProc] args type. It accepts only primitive values.

--- index.d.ts
+++ index.d.ts
@@ -1,30 +1,31 @@
+type primitiveTypes = string | number | undefined | boolean | bigint | null | symbol;
 interface Mp {
-	events: EventMpPool;
+    events: EventMpPool;
 
-	trigger(name: string, ...args: any[]): void;
-	invoke(name: string, ...args: any[]): void;
+    trigger(name: string, ...args: any[]): void;
+    invoke(name: string, ...args: any[]): void;
 }
 
 interface EventMpPool {
-	/**
-	 * This function calls registered Remote Prodecure Call (RPC) from browser to client-side and expects a callback.
-	 *
-	 * @param eventName Client name event you want to call.
-	 * @param args Data you want to send to client.
-	 * @returns Returns a promise
-	 *
-	 * @example
-	 * ```js
-	 * mp.events.callProc('test_proc', 'test');
-	 * ```
-	 *
-	 *
-	 */
-	callProc<T, K>(eventName: string, args: K): Promise<T>;
+    /**
+     * This function calls registered Remote Prodecure Call (RPC) from browser to client-side and expects a callback.
+     *
+     * @param eventName Client name event you want to call.
+     * @param args Data you want to send to client, it accepts only primitive-values.
+     * @returns Returns a promise
+     *
+     * @example
+     * ```js
+     * mp.events.callProc('test_proc', 'test');
+     * ```
+     *
+     *
+     */
+    callProc<T>(eventName: string, args: primitiveTypes): Promise<T>;
 }
 
 interface Window {
-	mp: Mp;
+    mp: Mp;
 }
 
 declare var mp: Mp;
