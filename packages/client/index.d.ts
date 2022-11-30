b9a96da258029d664a0f3d47a777c1539b8f79db
Marko Koprivanac <73706747+Kopraa@users.noreply.github.com>
2022-11-30 21:18:35 +0100

    taskScriptedAnimation arguments

--- packages/client/index.d.ts
+++ packages/client/index.d.ts
@@ -1984,7 +1984,7 @@ declare interface PedMpBase extends EntityMp {
 	taskRappelFromHeli(p1: number): void;
 	taskReactAndFlee(fleeTarget: Handle): void;
 	taskReloadWeapon(doReload: boolean): void;
-	taskScriptedAnimation(p1: any, p2: any, p3: any, p4: number, p5: number): void;
+	taskScriptedAnimation(lowData: number, midData: number, highData: number, blendIn: number, blendOut: number): void;
 	taskSeekCoverFrom(target: Handle, duration: number, p3: boolean): void;
 	taskSeekCoverToCoords(x1: number, y1: number, z1: number, x2: number, y2: number, z2: number, p7: any, p8: boolean): void;
 	taskSetBlockingOfNonTemporaryEvents(toggle: boolean): void;
