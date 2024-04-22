427365864f55e47f8aa655bfa0cd5d87d3c95b98
shr0x <aldodaka15@gmail.com>
2024-04-22 13:48:12 +0200

    added 'setCanPlayInCarIdles' to game ped

--- packages/client/game/ped.d.ts
+++ packages/client/game/ped.d.ts
@@ -849,6 +849,7 @@ declare interface GamePed extends GamePedLegacy {
 	setShouldPlayDirectedScenarioExit(ped: Handle, x: number, y: number, z: number): boolean;
 	createSynchronizedSceneAtMapObject(x: number, y: number, z: number, radius: number, objectHash: Hash): number;
 	setCanLosePropsOnDamage(ped: Handle, enable: boolean, flag: RageEnums.PropDamage): void;
+	setCanPlayInCarIdles(ped: Handle, enable: boolean): void;
 	unk: GamePedUnk;
 }
 
